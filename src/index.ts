6295ef8f6e297f118027e042bba97f05ff969ade
Peter Holló <hello@songsforthe.dev>
2022-02-18 17:46:39 +0100

    implement script functionality

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("test");
+import { writeFile as writeFileCb } from "fs";
+import { promisify } from "util";
+import { getItems, fetch } from "./utils";
+
+const writeFile = promisify(writeFileCb);
+
+const feedUrl = process.env.FEED_URL;
+if (feedUrl === undefined) {
+  throw new Error("environment variable FEED_ENV needs to be set");
+}
+
+async function main() {
+  const xml = (await fetch(feedUrl)) as string;
+  const items = getItems(xml);
+
+  await writeFile("./items.json", JSON.stringify(items), "utf-8");
+}
+
+main()
+  .then(() => console.log("done."))
+  .catch((err) => console.error(err));
